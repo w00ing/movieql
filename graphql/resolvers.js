3aa5cc5d030280f1086f53c44ae4bb3e6176709f
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-26 06:45:09 +0900

    Connect yts movie database to project

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,6 +3,8 @@ import { getMovies } from "./db";
 const resolvers = {
   Query: {
     movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovies(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
