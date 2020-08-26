10f3cc76724fc703cb078e8dfcbb783a3ec28353
Woo-yeong Kim <kimwy1997@gmail.com>
2020-08-27 04:09:41 +0900

    Adjust uri and db settings to connect to apollo-movies project

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,9 @@
-import { getMovies } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
     movies: (_, { rating, limit }) => getMovies(limit, rating),
-    movie: (_, { id }) => getMovies(id),
+    movie: (_, { id }) => getMovie(id),
     suggestions: (_, { id }) => getSuggestions(id),
   },
 };
